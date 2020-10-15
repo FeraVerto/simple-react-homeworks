8e446a532ed069407bfc73147a3f2118289a15fb
Клевакин Артем Николаевич <klevakin@tochka.com>
2020-10-15 20:36:10 +0500

    p2-homeworks-h1-message

---
+++ src/p2-homeworks/h1/HW1.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Message from "./Message";
+
+
+
+function HW1() {
+
+    const messageData = {
+        avatar: "https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg",
+        name: "Афанасий",
+        message: "React-Redux знать будешь круто!!!",
+        time: "02:15",
+    };
+
+    return (
+        <div>
+            <hr/>
+            homeworks 1
+
+            {/*should work (должно работать)*/}
+
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
+
+            <hr/>
+            {/*для личного творчества, могу проверить*/}
+            {/*<AlternativeMessage/>*/}
+            <hr/>
+        </div>
+    );
+}
+
+export default HW1;
---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import s from './Message.module.css';
+
+type HW1PropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: HW1PropsType) {
+    return (
+        <div className={s.message}>
+            <div className={s.avatar}>
+                <img src={props.avatar} alt=""/>
+            </div>
+            <div className={s.block}>
+                <div className={s.concave}>
+                    <div className={s.left_concave}></div>
+                </div>
+                <div className={s.text_block}>
+                    <div className={s.name}>{props.name}</div>
+                    <div className={s.message_text}>{props.message}</div>
+                    <div className={s.time}>{props.time}</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Message;
