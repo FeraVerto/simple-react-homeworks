9f02b05f520edf46af4661526cfaa2bb0d21e346
Клевакин Артем Николаевич <klevakin@tochka.com>
2020-12-08 22:00:50 +0500

    p2-homeworks-h8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,12 +1,3 @@
-/*const initialPeople = [
-    {_id: 0, name: "Кот", age: 3},
-    {_id: 1, name: "Александр", age: 66},
-    {_id: 2, name: "Коля", age: 16},
-    {_id: 3, name: "Виктор", age: 44},
-    {_id: 4, name: "Дмитрий", age: 40},
-    {_id: 5, name: "Ирина", age: 55},
-]*/
-
 import {initialPeopleType} from "../HW8";
 
 type actionType = {
